eab1640e2b29d42cf832b08da86b2398642ba477
arramos270 <57353507+arramos270@users.noreply.github.com>
2022-01-27 13:34:02 +0100

    Metodo a mitad de camino. No encuentra el album

--- src/test/java/com/sopromadze/blogapi/serviceTest/album/deleteAlbum.java
+++ src/test/java/com/sopromadze/blogapi/serviceTest/album/deleteAlbum.java
@@ -2,34 +2,37 @@ package com.sopromadze.blogapi.serviceTest.album;
 
 import com.sopromadze.blogapi.model.Album;
 import com.sopromadze.blogapi.model.user.User;
+import com.sopromadze.blogapi.payload.ApiResponse;
 import com.sopromadze.blogapi.payload.request.AlbumRequest;
 import com.sopromadze.blogapi.repository.AlbumRepository;
+import com.sopromadze.blogapi.repository.UserRepository;
 import com.sopromadze.blogapi.security.UserPrincipal;
 import com.sopromadze.blogapi.service.impl.AlbumServiceImpl;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.mockito.junit.jupiter.MockitoSettings;
-import org.mockito.quality.Strictness;
 import org.modelmapper.ModelMapper;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.test.context.support.WithMockUser;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
 
 import java.time.Instant;
 import java.util.Collections;
 
-@ExtendWith(MockitoExtension.class)
-@MockitoSettings(strictness = Strictness.LENIENT)
-public class deleteAlbum {
+import static org.mockito.Mockito.when;
 
-    @Autowired
-    private TestEntityManager testEntityManager;
+@ExtendWith(MockitoExtension.class)
+public class deleteAlbum {
 
     @Mock
     private AlbumRepository albumRepository;
+
+    @Mock
+    private UserRepository userRepository;
 
     @Mock
     private ModelMapper modelMapper;
@@ -41,28 +44,44 @@ public class deleteAlbum {
     entrada: albumService.deleteAlbum(albumid, UserPrincipal)
     salida esperada: un código 200, de haber borrado exitosamente el álbum */
     @Test
+    @WithMockUser(authorities = {"ROLE_ADMIN"})
     void deleteAlbum_success(){
+        //User creation
+        User u = new User(
+                "Frenando",
+                "Alonso",
+                "frenandoalonso20",
+                "frenandoalonso20@gmail.com",
+                "1234"); //Meter rol admin o no funciona
+        u.setId(1L);
+
         //UserPrincipal creation
-        UserPrincipal u = new UserPrincipal(
-                5L,
+        UserPrincipal userPrincipal = new UserPrincipal(
+                1L,
                 "Frenando",
                 "Alonso",
                 "frenandoalonso20",
                 "frenandoalonso20@gmail.com",
                 "1234",
-                Collections.emptyList()); //Meter rol admin o no funciona
+                Collections.emptyList()
+        );
 
         //AlbumRequest creation
         AlbumRequest a1 =new AlbumRequest();
         a1.setId(1L);
         a1.setTitle("Coches de carreras");
 
-        albumService.addAlbum(a1, u);
+        Album a =new Album();
+        a1.setId(1L);
+        a1.setTitle("Coches de carreras");
 
-        testEntityManager.persist(u);
-        testEntityManager.persist(a1);
+        a.setUser(userRepository.getUser(userPrincipal));
 
-        albumService.deleteAlbum(1L, u);
+        when(albumRepository.save(a)).thenReturn(a);
+
+        albumService.addAlbum(a1, userPrincipal);
+
+        assertEquals(HttpStatus.OK, albumService.deleteAlbum(1L,userPrincipal).getStatusCode());
 
     }
 }
