9a38ebfc38640f1b45112e0074ce6f19c1686367
alejandrosalesianos <alejandromartin2013@live.com>
2022-01-29 17:54:18 +0100

    Añadido cambios a FindByAlbumId

--- src/test/java/com/sopromadze/blogapi/repositoryTest/photo/FindByAlbumId.java
+++ src/test/java/com/sopromadze/blogapi/repositoryTest/photo/FindByAlbumId.java
@@ -10,15 +10,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ActiveProfiles;
 
 import java.time.Instant;
-import java.util.Arrays;
 
 import static org.junit.jupiter.api.Assertions.*;
 
