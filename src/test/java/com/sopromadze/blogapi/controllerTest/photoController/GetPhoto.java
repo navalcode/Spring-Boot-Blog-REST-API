5ce4e5d1f3ecda3bf95c1fd6c81c1fc77bd11c65
alejandrosalesianos <alejandromartin2013@live.com>
2022-01-30 18:11:20 +0100

    Añadido test del PhotoController GetPhoto

---
+++ src/test/java/com/sopromadze/blogapi/controllerTest/photoController/GetPhoto.java
@@ -0,0 +1,40 @@
+package com.sopromadze.blogapi.controllerTest.photoController;
+
+import com.sopromadze.blogapi.configurationSecurity.TestDisableSecurityConfig;
+import com.sopromadze.blogapi.service.PhotoService;
+import lombok.extern.java.Log;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@Log
+@SpringBootTest(classes = TestDisableSecurityConfig.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc
+
+public class GetPhoto {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private PhotoService photoService;
+
+    //Test: comprobar que devuelve un 200
+    //entrada: get("/api/photos/{id}",1
+    //salida esperada: el test funciona correctamente
+    @DisplayName("get photo")
+    @Test
+    void getPhoto_return200() throws Exception{
+
+        mockMvc.perform(get("/api/photos/{id}",1)
+                        .contentType("application/json"))
+                .andExpect(status().isOk());
+    }
+}
