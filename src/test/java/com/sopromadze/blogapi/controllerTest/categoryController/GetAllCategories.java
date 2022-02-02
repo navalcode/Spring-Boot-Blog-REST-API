de536742f750ceb1f2b650cae94a804ac3a06ef3
arramos270 <57353507+arramos270@users.noreply.github.com>
2022-02-02 10:28:12 +0100

    Terminado test GetAllCategories del controlador

--- src/test/java/com/sopromadze/blogapi/controllerTest/categoryController/GetAllCategories.java
+++ src/test/java/com/sopromadze/blogapi/controllerTest/categoryController/GetAllCategories.java
@@ -14,13 +14,18 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.data.domain.Page;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 
 import java.time.Instant;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -52,7 +57,7 @@ class GetAllCategories {
         cat1.setUpdatedAt(Instant.now());
 
         Category cat2 = new Category();
-        cat2.setName("Tecnología");
+        cat2.setName("Tecnologia");
         cat2.setCreatedAt(Instant.now());
         cat2.setUpdatedAt(Instant.now());
 
@@ -76,42 +81,54 @@ class GetAllCategories {
         resultadoVacio = new PagedResponse<>();
         resultadoVacio.setContent(listaVacia);
         resultadoVacio.setTotalPages(1);
-        resultadoVacio.setTotalElements(1);
+        resultadoVacio.setTotalElements(0);
         resultadoVacio.setLast(true);
-        resultadoVacio.setSize(1);
+        resultadoVacio.setSize(0);
 
         resultEmptyElement = new RestResponsePage(List.of(resultadoVacio));
         resultCompleteElement = new RestResponsePage(List.of(resultadoCompleto));
     }
 
     /*
-    Test:               Ver todas las categorias
-    Entrada:            get("/api/categories")
-    Salida esperada:    Lista de todas las categorias
+    Test: Ver todas las categorias
+    Entrada: GET("/api/categories")
+    Salida esperada: Lista de todas las categorias
     */
     @Test
     void getAllCategories_success() throws Exception {
 
-        mockMvc.perform(get("/api/categories")
+        PagedResponse<Category> expected = resultadoCompleto;
+
+        when(categoryService.getAllCategories(1, 1)).thenReturn(expected);
+
+        MvcResult mvcResult = mockMvc.perform(get("/api/categories")
                 .contentType("application/json")
                 .param("size", "1").param("page", "1")
                 .content(objectMapper.writeValueAsString(objectMapper.writeValueAsString(resultCompleteElement)))
         )
-                .andExpect(status().isOk());
+                .andExpect(status().isOk()).andReturn();
+
+        String actual = mvcResult.getResponse().getContentAsString();
+        String expectedJson = objectMapper.writeValueAsString(expected);
+        assertEquals(expectedJson, actual);
 
     }
 
     /*
-    Test:               Mandar una pagina sin categorias
-    Entrada:            get("/api/categories")
-    Salida esperada:    Pagina vacia
+    Test: Mandar una pagina sin sin elementos ni tamaño
+    Entrada: get("/api/categories")
+    Salida esperada: Pagina vacia sin elementos ni tamaño
     */
     @Test
     void getAllCategories_emptyCategoryPage() throws Exception {
 
+        PagedResponse<Category> voidExpected = resultadoVacio;
+
+        when(categoryService.getAllCategories(1, 0)).thenReturn(voidExpected);
+
         mockMvc.perform(get("/api/categories")
                 .contentType("application/json")
-                .param("size", "9").param("page", "1")
+                .param("size", "0").param("page", "1")
                 .content(objectMapper.writeValueAsString(objectMapper.writeValueAsString(resultEmptyElement)))
         )
                 .andExpect(result -> Page.empty());
